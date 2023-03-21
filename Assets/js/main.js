e718e5eee04665953b0d5b11e03111a02705ec89
Ali Mushrif <AliMushrif>
2023-03-22 02:35:03 +0800

    CONTACT

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -50,6 +50,7 @@ let swiperProjects = new Swiper(".projects__container", {
 });
 
 /*============ SWIPER TESTIMONIAL ============*/
+
 let swiperTestimonial = new Swiper(".testimonial__container", {
   grabCursor: true,
   navigation: {
@@ -57,3 +58,55 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+/*============ EMAIL JS ============*/
+
+const contactForm = document.getElementById("contact-form"),
+  contactName = document.getElementById("contact-name"),
+  contactEmail = document.getElementById("contact-email"),
+  contactProject = document.getElementById("contact-project"),
+  contactMessage = document.getElementById("contact-message");
+
+const sendEmail = (e) => {
+  e.preventDefault();
+  // Check if the field has a value
+  if (
+    contactName.value === "" ||
+    contactEmail.value === "" ||
+    contactProject.value === ""
+  ) {
+    // Add and remove color
+    contactMessage.classList.remove("color-blue");
+    contactMessage.classList.add("color-red");
+    // Show message
+    contactMessage.textContent = "Write all the input fields 📩";
+  } else {
+    // serviceID - templateID - #form - publicKey
+    emailjs
+      .sendForm(
+        "service_190brk8",
+        "template_ictxwis",
+        "#contact-form",
+        "PSMKD65Ik8tAB-ASw"
+      )
+      .then(
+        () => {
+          // Show message and add color
+          contactMessage.classList.add("color-blue");
+          contactMessage.textContent = "Message sent ✅";
+          // Remove message after five seconds
+          setTimeout(() => {
+            contactMessage.textContent = "";
+          }, 5000);
+        },
+        (error) => {
+          alert("OOPS! SOMETHING HAS FAILED...", error);
+        }
+      );
+    // To clear the input field
+    contactName.value = "";
+    contactEmail.value = "";
+    contactProject.value = "";
+  }
+};
+contactForm.addEventListener("submit", sendEmail);
