5c699a40a7462fefc7b1f947ffed08a6682d41cf
Ali Mushrif <AliMushrif>
2023-03-24 21:48:34 +0800

    Reveal_Animation

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -197,3 +197,23 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*============ SCROLL REVEAL ANIMATION ============*/
+
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true /* Animations repeat */,
+});
+
+sr.reveal(
+  `.home__data, .projects__container, .testimonial__container, .footer__container`
+);
+sr.reveal(`.home__info div`, { delay: 600, origin: "bottom", interval: 100 });
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {
+  origin: "left",
+});
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {
+  origin: "righ",
+});
+sr.reveal(`.qualification__content, .services__card`, { interval: 100 });
